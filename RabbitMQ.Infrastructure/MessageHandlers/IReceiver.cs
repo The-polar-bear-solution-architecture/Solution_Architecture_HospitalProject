2e9f819e9a190f69b3f081cfc5b3382950f4847c
Xin Wang <xx.wang@student.avans.nl>
2024-06-09 11:23:05 +0200

    Send temp solution

---
+++ RabbitMQ.Infrastructure/MessageHandlers/IReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Infrastructure.MessageHandlers
+{
+    public interface IReceiver
+    {
+        void Start();
+        void Stop();
+
+        void StartReceiving();
+    }
+}
---
+++ RabbitMQ.Infrastructure/MessageHandlers/RabbitMQReceiver.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Infrastructure.MessageHandlers
+{
+    public class RabbitMQReceiver : IReceiver
+    {
+        // 1. Making connection to rabbitMQ
+        // 2. Maintaining connection
+        // 3. Receiving messages from a queue.
+
+        public ConnectionFactory ConnectionFactory { get; set; }
+        public IConnection Connection { get; set; }
+        public IModel Model { get; set; }
+
+        public RabbitMQReceiver() {
+            ConnectionFactory = new ConnectionFactory { HostName = "localhost" };
+            Connection = ConnectionFactory.CreateConnection();
+            Model = Connection.CreateModel();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartReceiving()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
