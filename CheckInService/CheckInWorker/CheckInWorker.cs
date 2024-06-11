d20804dc7fb0c3618378518e80bccfc54024a312
Xin Wang <xx.wang@student.avans.nl>
2024-06-11 16:24:36 +0200

    Create some base classes for receiving messages

---
+++ CheckInService/CheckInWorker/CheckInWorker.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Messages.Interfaces;
+
+namespace CheckInService.CheckInWorker
+{
+    public class CheckInWorker : IMessageHandleCallback, IHostedService
+    {
+        public Task<bool> HandleMessageAsync(string messageType, object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
