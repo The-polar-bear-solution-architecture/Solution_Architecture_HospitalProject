1a825b674dfedf42d112cd2289cb425a6756c704
Xin Wang <xx.wang@student.avans.nl>
2024-06-11 16:00:18 +0200

    Create the event and command classes required to perform event-driven architecture

---
+++ RabbitMQ.Messages/Messages/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Messages.Messages
+{
+    public class Command: Message
+    {
+        public Command(Guid messageId) : base(messageId)
+        {
+        }
+
+        public Command(string messageType) : base(messageType)
+        {
+        }
+
+        public Command(Guid messageId, string messageType) : base(messageId, messageType)
+        {
+        }
+    }
+}
