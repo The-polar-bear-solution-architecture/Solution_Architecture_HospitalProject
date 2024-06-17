5f09c9f94238ed475ee10c6ecc33637a1216f2c1
Xin Wang <xx.wang@student.avans.nl>
2024-06-17 21:53:33 +0200

    Revert back connection string in apointmentservice

--- AppointmentService/DB/AppointmentServiceContext.cs
+++ AppointmentService/DB/AppointmentServiceContext.cs
@@ -16,12 +16,10 @@ namespace AppointmentService.DB
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Physician> Physicians { get; set; }
         //public DbSet<Role> Roles { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString = "Data Source=localhost;Initial Catalog=AppointmentService;User ID=sa;Password=Rick@Sanchez;Trust Server Certificate=True";
-            string connectionStringJascha = "Data Source=.;Initial Catalog=AppointmentService;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+        
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=AppointmentService;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+        //}
     }
 }
