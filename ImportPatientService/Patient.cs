f6406354a59100bcbaa7df07669265bd15b1535e
Tristan Schuring <tristandevelop@protonmail.com>
2024-06-18 12:11:06 +0200

    Import patient service setup

---
+++ ImportPatientService/Patient.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportPatientService
+{
+    public class Patient
+    {
+        [Name("Company Name")]
+        public string CompanyName { get; set; }
+        [Name("First Name")]
+        public string FirstName { get; set; }
+        [Name("Last Name")]
+        public string LastName { get; set; }
+        [Name("Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Name("Address")]
+        public string Address { get; set; }
+        public static Patient FromCSV(string csvLine)
+        {
+
+            string[] values = csvLine.Split(',');
+            return new Patient() { FirstName = values[1], LastName = values[2], PhoneNumber = values[3], Address = values[4] };
+        }
+    }
+
+}
---
+++ ImportPatientService/Program.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using ImportPatientService;
+using System.Formats.Asn1;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Globalization;
+using RabbitMQ.Messages.Configuration;
+using RabbitMQ.Messages.Interfaces;
+using RabbitMQ.Infrastructure.MessagePublishers;
+
+
+IHost host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        // services.UseRabbitMQMessagePublisher(hostContext.Configuration);
+        services.AddHostedService<HostingWorker>();
+    })
+    .UseConsoleLifetime()
+    .Build();
+await host.RunAsync();
