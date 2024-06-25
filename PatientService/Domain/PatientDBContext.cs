4b8272d2dd9e8b169518f10d41520eb29a6521d5
Xin Wang <xx.wang@student.avans.nl>
2024-06-25 21:12:02 +0200

    Push seed data to patient service

--- PatientService/Domain/PatientDBContext.cs
+++ PatientService/Domain/PatientDBContext.cs
@@ -13,6 +13,14 @@ namespace PatientService.Domain
         public DbSet<Patient> Patients { get; set; }
         public DbSet<GeneralPractitioner> GeneralPractitioners { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Create a default general practicioner.
+            GeneralPractitioner generalPractitioner = new GeneralPractitioner() { Email = "", FirstName = "Tristan", LastName = "Schuring",  Address = "Example street 123", PhoneNumber = "0312312413", Id = Guid.Parse("99463e24-c0cc-45d2-9b97-3e23e3556de3") };
+            modelBuilder.Entity<GeneralPractitioner>().HasData(generalPractitioner);
+        }
+
         public void MigrateDB()
         {
             Policy
---
+++ PatientService/Migrations/20240625191110_create_default_gp.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientService.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_default_gp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "GeneralPractitionerId",
+                table: "Patients",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "GeneralPractitioners",
+                columns: new[] { "Id", "Address", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { new Guid("99463e24-c0cc-45d2-9b97-3e23e3556de3"), "Example street 123", "", "Tristan", "Schuring", "0312312413" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients",
+                column: "GeneralPractitionerId",
+                principalTable: "GeneralPractitioners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients");
+
+            migrationBuilder.DeleteData(
+                table: "GeneralPractitioners",
+                keyColumn: "Id",
+                keyValue: new Guid("99463e24-c0cc-45d2-9b97-3e23e3556de3"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "GeneralPractitionerId",
+                table: "Patients",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients",
+                column: "GeneralPractitionerId",
+                principalTable: "GeneralPractitioners",
+                principalColumn: "Id");
+        }
+    }
+}
