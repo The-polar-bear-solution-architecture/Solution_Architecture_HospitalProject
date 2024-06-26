62c118062c8e416fa9d42f43cdbcf37b302d91e0
JaschavdA <94369567+JaschavdA@users.noreply.github.com>
2024-06-26 14:06:58 +0200

    Split AppointmentService Database into read and write DB

---
+++ AppointmentService/DB/AppointmentReadServiceContext.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace AppointmentService.DB
+{
+    public class AppointmentReadServiceContext : DbContext
+    {
+        public DbSet<AppointmentRead> appointmentsRead { get; set; }
+
+        public AppointmentReadServiceContext(DbContextOptions<AppointmentReadServiceContext> options) : base(options) { }
+
+        public void MigrateDB()
+        {
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetry(10, r => TimeSpan.FromSeconds(10))
+                .Execute(() => Database.Migrate());
+        }
+    }
+}
--- AppointmentService/Migrations/20240626120003_removed-read-model.Designer.cs
+++ AppointmentService/Migrations/20240626120003_removed-read-model.Designer.cs
@@ -4,6 +4,7 @@ using AppointmentService.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppointmentService.Migrations
 {
     [DbContext(typeof(AppointmentServiceContext))]
-    partial class AppointmentServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20240626120003_removed-read-model")]
+    partial class removedreadmodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
